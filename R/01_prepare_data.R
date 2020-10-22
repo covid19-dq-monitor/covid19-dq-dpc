5732dbe9cb18ddaf58f483f73839d194d05f5807
covid19-dq-monitor <lorenzo.veg.ro@gmail.com>
2020-10-22 18:45:35 +0200

    Added time check to data preparation

--- R/01_prepare_data.R
+++ R/01_prepare_data.R
@@ -12,34 +12,50 @@ cols <- cols(
   casi_testati = col_integer(),
   note = col_character()
 )
-regioni_raw <- 
-  readr::read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv',
-                  col_types = cols)
-today <- max(regioni_raw$data)
+# dummy date to engage the cycle
+today <- as.Date("1900-01-01")
+
+while(today != Sys.Date()) {
+  Sys.sleep(60)
+  regioni_raw <-
+    readr::read_csv(
+      'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv',
+      col_types = cols
+    )
+  
+  today <- max(regioni_raw$data)
+}
 
 #for raw
-raw <- regioni_raw %>% 
+raw <- regioni_raw %>%
   filter(data == today)
 # for delta
 delta <- regioni_raw %>%
-  filter(between(data, today - as.difftime(14, unit="days"), today))%>% 
-  arrange(denominazione_regione, data) %>% 
-  group_by(denominazione_regione) %>% 
-  mutate(delta_ricoverati = ricoverati_con_sintomi - lag(ricoverati_con_sintomi),
-         delta_terapia_intensiva = terapia_intensiva - lag(terapia_intensiva),
-         delta_ospedalizzati = totale_ospedalizzati - lag(totale_ospedalizzati),
-         delta_isolamento_domiciliare = isolamento_domiciliare - lag(isolamento_domiciliare),
-         delta_casi_testati = casi_testati - lag(casi_testati),
-         delta_deceduti = deceduti - lag(deceduti),
-         delta_casi_screening = casi_da_screening - lag(casi_da_screening),
-         delta_casi_sospetto = casi_da_sospetto_diagnostico - lag(casi_da_sospetto_diagnostico),
-         delta_totale_positivi = totale_positivi - lag(totale_positivi),
-         delta_dimessi_guariti = dimessi_guariti - lag(dimessi_guariti),
-         delta_totale_casi = totale_casi - lag(totale_casi),
-         delta_tamponi = tamponi- lag(tamponi)) %>% 
-  select(data, denominazione_regione,
-         ricoverati_con_sintomi, terapia_intensiva, totale_ospedalizzati,
-         starts_with("delta"))
+  filter(between(data, today - as.difftime(14, unit = "days"), today)) %>%
+  arrange(denominazione_regione, data) %>%
+  group_by(denominazione_regione) %>%
+  mutate(
+    delta_ricoverati = ricoverati_con_sintomi - lag(ricoverati_con_sintomi),
+    delta_terapia_intensiva = terapia_intensiva - lag(terapia_intensiva),
+    delta_ospedalizzati = totale_ospedalizzati - lag(totale_ospedalizzati),
+    delta_isolamento_domiciliare = isolamento_domiciliare - lag(isolamento_domiciliare),
+    delta_casi_testati = casi_testati - lag(casi_testati),
+    delta_deceduti = deceduti - lag(deceduti),
+    delta_casi_screening = casi_da_screening - lag(casi_da_screening),
+    delta_casi_sospetto = casi_da_sospetto_diagnostico - lag(casi_da_sospetto_diagnostico),
+    delta_totale_positivi = totale_positivi - lag(totale_positivi),
+    delta_dimessi_guariti = dimessi_guariti - lag(dimessi_guariti),
+    delta_totale_casi = totale_casi - lag(totale_casi),
+    delta_tamponi = tamponi - lag(tamponi)
+  ) %>%
+  select(
+    data,
+    denominazione_regione,
+    ricoverati_con_sintomi,
+    terapia_intensiva,
+    totale_ospedalizzati,
+    starts_with("delta")
+  )
 
 summaries <- delta %>%
   group_by(denominazione_regione) %>% 
